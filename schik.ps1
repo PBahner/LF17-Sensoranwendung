e39fea3231b95f665883ecc2aa5589a5bb0385a2
PBahner <kl.timo@outlook.de>
2025-01-07 12:32:18 +0100

    Added install scripts

---
+++ schik.ps1
@@ -0,0 +1,14 @@
+﻿$username=Read-Host -Prompt "Enter Github Username"
+$key=Read-Host -Prompt "Enter Github Key"
+$githubCloneUrl=-join("https://", $username, ":", $key, "@github.com/PBahner/LF17-Sensoranwendung.git")
+set HTTP_PROXY=kjs-04.lan.dd-schulen.de:3128
+set HTTPS_PROXY=kjs-04.lan.dd-schulen.de:3128
+git config --global http.sslbackend openssl
+git config --global http.proxy http://kjs-04.lan.dd-schulen.de:3128
+git clone $githubCloneUrl
+Set-Location -Path .\LF17-Sensoranwendung
+python -m venv venv
+.\venv\Scripts\Activate.ps1
+pip install -e python-app[dev]
+pip install -r mkdocs\requirements.txt
+pause
